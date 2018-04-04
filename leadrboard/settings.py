01eaa7405708ba90f63aed589b1ee2e46ff112a0
Sidhant <sidhant@Sidhants-MacBook-Pro.local>
2018-04-04 18:17:20 -0400

    hosts added

--- leadrboard/settings.py
+++ leadrboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [".herokuapp.com",'heroku.com', 'hereokuapp.com',"leadrboard.herokuapp.com", "*.herokuapp.com"]
+ALLOWED_HOSTS = [".herokuapp.com",'heroku.com', 'hereokuapp.com',"leadrboard.herokuapp.com", "*.herokuapp.com","https://heroku.com/"]
 
 
 # Application definition
