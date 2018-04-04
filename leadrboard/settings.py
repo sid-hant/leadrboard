9e07c81537f0d99d65a7f214dd2418a313540a93
Sidhant <sidhant@Sidhants-MacBook-Pro.local>
2018-04-04 19:25:05 -0400

    TRUE

--- leadrboard/settings.py
+++ leadrboard/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", False)
+DEBUG = os.environ.get("DEBUG", True)
 
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')
