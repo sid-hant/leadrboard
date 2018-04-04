bdf059b15a399e2286208ff10d338d467949760a
Sidhant <sidhant@Sidhants-MacBook-Pro.local>
2018-04-04 18:22:54 -0400

    debug = False

--- leadrboard/settings.py
+++ leadrboard/settings.py
@@ -23,7 +23,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com",'heroku.com', 'hereokuapp.com',"leadrboard.herokuapp.com", "*.herokuapp.com","https://heroku.com/"]
 
