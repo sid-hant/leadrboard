6403a25abe4780b75a9b65e69885a18e65b878c2
Sidhant <sidhant@Sidhants-MacBook-Pro.local>
2018-04-04 19:39:24 -0400

    host

--- leadrboard/settings.py
+++ leadrboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", False)
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com']
 #env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
 
 # Application definition
