3645fc6467d52231bf15210e3eecbd674070ee79
Sidhant <sidhant@Sidhants-MacBook-Pro.local>
2018-04-05 19:24:41 -0400

    Debug = False

--- leadrboard/settings.py
+++ leadrboard/settings.py
@@ -23,9 +23,9 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", True)
+DEBUG = os.environ.get("DEBUG", False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['wwww.heroku.com', '.herokuapp.com']
 
 # Application definition
 
