4422abd363e101f7dc87ed11daf4a46ee4e9a5f1
Sidhant <sidhant@Sidhants-MacBook-Pro.local>
2018-04-07 12:30:05 -0400

    database changes

--- leadrboard/settings.py
+++ leadrboard/settings.py
@@ -77,17 +77,16 @@ WSGI_APPLICATION = 'leadrboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -133,3 +132,6 @@ LOGOUT_REDIRECT_URL = "home"
 LOGIN_URL = "home"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
