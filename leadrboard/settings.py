e948032fcea8edf7857ec2deb96293c0ed9213a4
Sidhant <sidhant@Sidhants-MacBook-Pro.local>
2018-04-07 12:47:13 -0400

    database changes

--- leadrboard/settings.py
+++ leadrboard/settings.py
@@ -133,7 +133,5 @@ LOGIN_URL = "home"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-'''
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-'''
